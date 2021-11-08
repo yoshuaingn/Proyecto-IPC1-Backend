ea4c55db9bf8d3a0ffbdc68799e293b1d3ff7afb
yoshua2712 <yoshuatecun7@gmail.com>
2021-11-07 20:05:45 -0600

    s

--- Gestor.py
+++ Gestor.py
@@ -71,7 +71,7 @@ class Gestor:
     
     def Genera1(self):
         from reportlab.lib.pagesizes import A4
-        doc = SimpleDocTemplate("Reporte_Usuarios.pdf", pagesize = A4)
+        doc = SimpleDocTemplate("C:/Users/"+getuser()+"/Downloads/Reporte_Usuarios.pdf", pagesize = A4)
         story=[]
         datos = []
         for i in range(0,len(self.mivector)):
