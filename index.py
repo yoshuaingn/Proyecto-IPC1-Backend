03d08c1ec4b4eb0f333e1c60ee55879dc26b7a80
yoshua2712 <yoshuatecun7@gmail.com>
2021-11-07 19:30:27 -0600

    a

--- index.py
+++ index.py
@@ -76,7 +76,7 @@ def elimanaapp1():
         return "{\"data\":\"false\"}"
 
 #genera el pdf Users
-@app.route('/pdf1', methods=['POST'])
+@app.route('/pdf1', methods=['POST','GET'])
 def GeneraPDF1():
     return gestor.Genera1()
 
